80991a82742b02576ea266c399f927608dc4fee3
Preet Dua <615318+prabhpreet@users.noreply.github.com>
2022-06-18 21:27:01 +0530

    Basic health check

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-//#[get("/hello/{name}")]
-async fn greet(req:HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {name}!")
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 //#[actix_web::main] // or 
@@ -11,8 +9,7 @@ async fn greet(req:HttpRequest) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-        .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
