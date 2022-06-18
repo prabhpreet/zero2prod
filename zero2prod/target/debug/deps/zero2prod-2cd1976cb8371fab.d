608db4abb6e76b10586137c5e5606a5cbcb73f2b
Preet Dua <615318+prabhpreet@users.noreply.github.com>
2022-06-18 20:02:43 +0530

    Moved contents to root folder

--- zero2prod/target/debug/deps/zero2prod-2cd1976cb8371fab.d
+++
@@ -1,7 +0,0 @@
-c:\Users\auhso\Documents\Development\Rust\mob\mobexplorezero2prod\zero2prod\target\debug\deps\zero2prod-2cd1976cb8371fab.rmeta: src\main.rs
-
-c:\Users\auhso\Documents\Development\Rust\mob\mobexplorezero2prod\zero2prod\target\debug\deps\zero2prod-2cd1976cb8371fab.d: src\main.rs
-
-src\main.rs:
-
-# env-dep:CLIPPY_ARGS=
--- zero2prod/target/debug/deps/zero2prod-6f7bf58f25f227af.d
+++
@@ -1,7 +0,0 @@
-c:\Users\auhso\Documents\Development\Rust\mob\mobexplorezero2prod\zero2prod\target\debug\deps\zero2prod-6f7bf58f25f227af.rmeta: src\main.rs
-
-c:\Users\auhso\Documents\Development\Rust\mob\mobexplorezero2prod\zero2prod\target\debug\deps\zero2prod-6f7bf58f25f227af.d: src\main.rs
-
-src\main.rs:
-
-# env-dep:CLIPPY_ARGS=
