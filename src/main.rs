d9422fd47e82b73213403cc018910559fda41503
pekasus <josh@pekasys.com>
2022-07-10 11:56:53 -0400

    passed first CI test

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use mobexplorezero2prod::run;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-//#[actix_web::main] // or 
+//#[actix_web::main] // or
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    run()?.await
 }
