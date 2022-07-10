716d05fdeae0092e917151f8e81cd298edf9653b
Peter Kehl <peter.kehl@gmail.com>
2022-07-10 09:21:29 -0700

    Tests use a random TCP port.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use mobexplorezero2prod::run;
+use std::net::TcpListener;
 
 //#[actix_web::main] // or
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    run(listener)?.await
 }
