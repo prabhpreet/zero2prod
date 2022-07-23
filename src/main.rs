c591ca124b94ee9895a5a33f743194b58a29d6c1
HsienHsiu <hsien_hsiu.liao@yahoo.com>
2022-07-23 09:45:03 -0700

    working on INSERT query 3.6.4

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use mobexplorezero2prod::{configuration::get_configuration, startup::run};
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 //#[actix_web::main] // or
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
 
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
