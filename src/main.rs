2677f8cbfb0ec1c2764e4914644db960a916cc80
Peter Kehl <peter.kehl@gmail.com>
2022-07-27 07:23:04 -0700

    Starting withn env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use mobexplorezero2prod::{configuration::get_configuration, startup::run};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -5,6 +6,8 @@ use std::net::TcpListener;
 //#[actix_web::main] // or
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,8 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
-use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -11,7 +11,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-        // Middlewares are added using the `wrap` method on `App`
+            // Middlewares are added using the `wrap` method on `App`
             .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
