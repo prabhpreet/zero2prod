f4fddbf8295212e25f7cb569fcf34b4a6ba90f44
Peter Kehl <peter.kehl@gmail.com>
2022-07-30 08:48:14 -0700

    Starting with .instrument(..)

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,8 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
-use tracing::{error, info};
 use sqlx::PgPool;
+use tracing::Instrument;
+use tracing::{error, info};
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -18,13 +19,13 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         subscriber_email = %form.email,
         subscriber_name = %form.name
     );
-    
+
     let _request_span_guard = request_span.enter();
 
-    tracing::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
-    );
+    // We do not call `.enter` on query_span!
+    // `.instrument` takes care of it at the right moments
+    // in the query future lifetime
+    let query_span = tracing::info_span!(" Saving new subscriber details in the database");
 
     match sqlx::query!(
         r#"
@@ -37,17 +38,23 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now()
     )
     .execute(pool.get_ref())
+    // First we attach the instrumentation, then we `.await` it
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
+            /*
+            // this was active before introducing .instrument(...)
             tracing::info!(
                 "request_id {} - New subscriber details have been saved",
                 request_id
-            );
+            );*/
 
             HttpResponse::Ok().finish()
         }
         Err(e) => {
+            // Yes, this error log falls outside of `query_span`
+            // We'll rectify it later, pinky swear!
             tracing::error!(
                 "request_id {} - Failed to execute query: {:?}",
                 request_id,
