1d5a8b9f094f8a14715a21cf033901e5c0726933
pekasus <josh@pekasys.com>
2022-08-04 09:47:08 -0500

    created insert_subscriber and added tracing instrument 5.12

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -57,7 +57,14 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let query_span = tracing::info_span!(" Saving new subscriber details in the database");
 
-    match sqlx::query!(
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -67,15 +74,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
+    .execute(pool)
     // First we attach the instrumentation, then we `.await` it
-    .instrument(query_span)
     .await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            tracing::error!("Failed to execute query: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|err| {
+        tracing::error!("Fail to execute query: {:?}", err);
+        err
+    })?;
+    Ok(())
 }
