011cec9efabac69d5b00f5de3732e9950ae5d75c
Preet Dua <615318+prabhpreet@users.noreply.github.com>
2022-08-21 21:50:32 +0530

    Finished Chapter 6.5

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,9 @@
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
+use actix_web::web::Form;
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use std::convert::TryFrom;
 use tracing::Instrument;
 use tracing::{error, info};
 use unicode_segmentation::UnicodeSegmentation;
@@ -47,6 +49,15 @@ pub struct FormData {
     name: String,
 }
 
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
+}
+
 #[tracing::instrument(
     name = "Adding a new subscriber",
     skip(form, pool),
@@ -55,15 +66,15 @@ pub struct FormData {
         subscriber_name = %form.name
     )
 )]
+
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     //let subscriber_name = crate::domain::SubscriberName(form.name.clone());
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name,
+
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
+        Err(_) => {
+            return HttpResponse::BadRequest().finish();
+        }
     };
     /*if !is_valid_name(&form.name) {
         return HttpResponse::BadRequest().finish();
@@ -88,7 +99,7 @@ pub async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
