7b114f89e86bb543b955b6fb7828e2a2e66abfaa
pekasus <josh@pekasys.com>
2022-09-18 16:04:27 -0500

    up to Timeouts

---
+++ tests/api/health_check.rs
@@ -0,0 +1,21 @@
+use crate::helpers::{spawn_app, TestApp};
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let TestApp { address, .. } = spawn_app().await;
+    // We need to bring in `reqwest`
+    // to perform HTTP requests against our application.
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
