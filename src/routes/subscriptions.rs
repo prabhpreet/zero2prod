72bd8308ac8880aeafbe8efceb4080ddbbef0bd0
prabhpreet <615318+prabhpreet@users.noreply.github.com>
2022-10-23 07:48:59 -0600

    Zero downtime deployments

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -8,6 +8,7 @@ use tracing::Instrument;
 use tracing::{error, info};
 use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
+use crate::email_client::EmailClient;
 
 /*
 //Experimenting with 'static - see https://docs.rs/tracing/latest/tracing/trait.Subscriber.html
@@ -60,14 +61,18 @@ impl TryFrom<FormData> for NewSubscriber {
 
 #[tracing::instrument(
     name = "Adding a new subscriber",
-    skip(form, pool),
+    skip(form, pool, email_client),
     fields(
         subscriber_email = %form.email,
         subscriber_name = %form.name
     )
 )]
 
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+pub async fn subscribe(
+    form: web::Form<FormData>, 
+    pool: web::Data<PgPool>,
+    email_client: web::Data<EmailClient>,
+) -> HttpResponse {
     //let subscriber_name = crate::domain::SubscriberName(form.name.clone());
 
     let new_subscriber = match form.0.try_into() {
@@ -79,10 +84,24 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     /*if !is_valid_name(&form.name) {
         return HttpResponse::BadRequest().finish();
     }*/
-    match insert_subscriber(&pool, &new_subscriber).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish(),
+    if insert_subscriber(&pool, &new_subscriber).await.is_err() {
+        return HttpResponse::InternalServerError().finish();
     }
+
+    if email_client
+        .send_email(
+            new_subscriber.email, 
+            "Welcome!", 
+            "Welcome to our newsletter!", 
+            "Welcome to our newsletter!"
+        )
+        .await
+        .is_err()
+        {
+            return HttpResponse::InternalServerError().finish();
+        }
+    HttpResponse::Ok().finish()
+
 }
 
 #[tracing::instrument(
